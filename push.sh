7dc0a1fb6cb8a8e438c5443851f6242191627485
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2018-05-07 10:37:31 +0100

    included push for tomcat image

--- push.sh
+++ push.sh
@@ -7,7 +7,7 @@ source params.sh
 docker push $BASE/rdkit-build:$TAG
 docker push $BASE/rdkit-python-debian:$TAG
 docker push $BASE/rdkit-java-debian:$TAG
-#docker push $BASE/rdkit-tomcat-debian:$TAG
+docker push $BASE/rdkit-tomcat-debian:$TAG
 
 echo "Images pushed using tag $TAG"
 
