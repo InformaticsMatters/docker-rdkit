9d4493ea2e407596dc82bfee121969f5a614f815
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2018-06-10 10:34:47 +0100

    corrected push.sh

--- push.sh
+++ push.sh
@@ -4,10 +4,10 @@ set -e
 
 source params.sh
 
-docker push $BASE/rdkit-build:$TAG
-docker push $BASE/rdkit-python-debian:$TAG
-docker push $BASE/rdkit-java-debian:$TAG
-docker push $BASE/rdkit-tomcat-debian:$TAG
+docker push $BASE/rdkit-build:$DOCKER_TAG
+docker push $BASE/rdkit-python-debian:$DOCKER_TAG
+docker push $BASE/rdkit-java-debian:$DOCKER_TAG
+docker push $BASE/rdkit-tomcat-debian:$DOCKER_TAG
 
-echo "Images pushed using tag $TAG"
+echo "Images pushed using tag $DOCKER_TAG"
 
