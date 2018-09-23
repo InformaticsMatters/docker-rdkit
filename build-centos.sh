6d3c6e253c4bef8d98c950bacefc7f821c9b0e7d
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2018-09-23 15:46:51 +0100

    attempts at centos and cartridge builds - wip

--- build-centos.sh
+++ build-centos.sh
@@ -8,32 +8,34 @@ source params.sh
 
 # build RDKit
 docker build -f Dockerfile-build-centos\
-  -t $BASE/rdkit-build:$TAG\
-  --build-arg RDKIT_BRANCH=$BRANCH .
+  -t $BASE/rdkit-build:$DOCKER_TAG\
+  --build-arg RDKIT_BRANCH=$GIT_BRANCH .
 
 # copy the packages
-rm -rf artifacts/$TAG
-mkdir -p artifacts/$TAG
-mkdir artifacts/$TAG/rpms
-mkdir artifacts/$TAG/java
+rm -rf artifacts/centos/$DOCKER_TAG
+mkdir -p artifacts/centos/$DOCKER_TAG/rpms
+mkdir -p artifacts/centos/$DOCKER_TAG/java
+mkdir -p artifacts/centos/$DOCKER_TAG/boost
 docker run -it --rm -u $(id -u)\
-  -v $PWD/artifacts/$TAG:/tohere:Z\
-  $BASE/rdkit-build:$TAG bash -c 'cp build/*.rpm /tohere/rpms && cp Code/JavaWrappers/gmwrapper/org.RDKit.jar /tohere/java && cp Code/JavaWrappers/gmwrapper/libGraphMolWrap.so /tohere/java'
+  -v $PWD/artifacts/centos/$DOCKER_TAG:/tohere:Z\
+  $BASE/rdkit-build:$DOCKER_TAG bash -c 'cp build/*.rpm /tohere/rpms && cp /root/boost-1.56.0.tgz /tohere/boost'
+
+# cp Code/JavaWrappers/gmwrapper/org.RDKit.jar /tohere/java && cp Code/JavaWrappers/gmwrapper/libGraphMolWrap.so /tohere/java && 
 
 # build image for python
 docker build -f Dockerfile-python-centos\
-  -t $BASE/rdkit-python-centos:$TAG\
-  --build-arg TAG=$TAG .
-echo "Built image informaticsmatters/rdkit-python-centos:$TAG"
+  -t $BASE/rdkit-python-centos:$DOCKER_TAG\
+  --build-arg TAG=$DOCKER_TAG .
+echo "Built image informaticsmatters/rdkit-python-centos:$DOCKER_TAG"
 
 # build image for java
-docker build -f Dockerfile-java-centos\
-  -t $BASE/rdkit-java-centos:$TAG\
-  --build-arg TAG=$TAG .
-echo "Built image informaticsmatters/rdkit-java-centos:$TAG"
+#docker build -f Dockerfile-java-centos\
+#  -t $BASE/rdkit-java-centos:$DOCKER_TAG\
+#  --build-arg TAG=$DOCKER_TAG .
+#echo "Built image informaticsmatters/rdkit-java-centos:$DOCKER_TAG"
 
 # build image for tomcat
-docker build -f Dockerfile-tomcat-centos\
-  -t $BASE/rdkit-tomcat-centos:$TAG\
-  --build-arg TAG=$TAG .
-echo "Built image informaticsmatters/rdkit-java-centos:$TAG"
+#docker build -f Dockerfile-tomcat-centos\
+#  -t $BASE/rdkit-tomcat-centos:$DOCKER_TAG\
+#  --build-arg TAG=$DOCKER_TAG .
+#echo "Built image informaticsmatters/rdkit-java-centos:$DOCKER_TAG"
--- build-debian.sh
+++ build-debian.sh
@@ -11,26 +11,19 @@ docker build -f Dockerfile-build-debian\
   --build-arg GIT_TAG=$GIT_TAG .
 
 # copy the packages
-rm -rf artifacts/$DOCKER_TAG
-mkdir -p artifacts/$DOCKER_TAG
-mkdir artifacts/$DOCKER_TAG/debs
-mkdir artifacts/$DOCKER_TAG/rpms
-mkdir artifacts/$DOCKER_TAG/java
+rm -rf artifacts/debian/$DOCKER_TAG
+mkdir -p artifacts/debian/$DOCKER_TAG
+mkdir -p artifacts/debian/$DOCKER_TAG/debs
+mkdir -p artifacts/debian/$DOCKER_TAG/java
 docker run -it --rm -u $(id -u)\
-  -v $PWD/artifacts/$DOCKER_TAG:/tohere:Z\
-  $BASE/rdkit-build:$DOCKER_TAG bash -c 'cp build/*.deb /tohere/debs && cp build/*.rpm /tohere/rpms && cp Code/JavaWrappers/gmwrapper/org.RDKit.jar /tohere/java && cp Code/JavaWrappers/gmwrapper/libGraphMolWrap.so /tohere/java'
+  -v $PWD/artifacts/debian/$DOCKER_TAG:/tohere:Z\
+  $BASE/rdkit-build:$DOCKER_TAG bash -c 'cp build/*.deb /tohere/debs && cp Code/JavaWrappers/gmwrapper/org.RDKit.jar /tohere/java && cp Code/JavaWrappers/gmwrapper/libGraphMolWrap.so /tohere/java'
 
 # build image for python on debian
 docker build -f Dockerfile-python-debian\
   -t $BASE/rdkit-python-debian:$DOCKER_TAG\
   --build-arg DOCKER_TAG=$DOCKER_TAG .
 echo "Built image informaticsmatters/rdkit-python-debian:$DOCKER_TAG"
-
-# build image for python on centos
-docker build -f Dockerfile-python-centos\
-  -t $BASE/rdkit-python-centos:$DOCKER_TAG\
-  --build-arg DOCKER_TAG=$DOCKER_TAG .
-echo "Built image informaticsmatters/rdkit-python-centos:$DOCKER_TAG"
 
 # build image for java on debian
 docker build -f Dockerfile-java-debian\
