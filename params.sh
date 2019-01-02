699083cd8ffde1f8e5823f3bed57415ca8ffc538
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2019-01-02 19:42:21 +0000

    handle problems with java images and install for postgres 10

--- params.sh
+++ params.sh
@@ -1,6 +1,7 @@
 export DOCKER_TAG=latest
 export GIT_REPO=https://github.com/rdkit/rdkit.git
 export GIT_BRANCH=master
+unset GIT_TAG
 
 export BASE=informaticsmatters
 echo "DOCKER_TAG=$DOCKER_TAG GIT_BRANCH=$GIT_BRANCH no tag"
