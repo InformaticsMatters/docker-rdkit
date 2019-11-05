e5faa91096a85ae29f4070fd8781e728e85f6eca
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2019-11-05 15:19:39 +0000

    Release_2019_09_1 build

--- params.sh
+++ params.sh
@@ -1,7 +1,7 @@
-export DOCKER_TAG=latest
+export DOCKER_TAG=Release_2019_09_1
 export GIT_REPO=https://github.com/rdkit/rdkit.git
-export GIT_BRANCH=master
-unset GIT_TAG
+export GIT_BRANCH=Release_2019_09
+export GIT_TAG=$DOCKER_TAG
 
 export BASE=informaticsmatters
-echo "DOCKER_TAG=$DOCKER_TAG GIT_BRANCH=$GIT_BRANCH no tag"
+echo "DOCKER_TAG=$DOCKER_TAG GIT_BRANCH=$GIT_BRANCH GIT_TAG=$GIT_TAG"
