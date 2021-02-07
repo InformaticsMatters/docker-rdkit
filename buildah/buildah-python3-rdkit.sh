0aab6b5915f72ba2b3cbc319fd7c27bb954b8625
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2021-02-07 14:04:55 +0000

    buildah updates

--- buildah/buildah-python3-rdkit.sh
+++ buildah/buildah-python3-rdkit.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-# Build script for RDKit with Python3.
-# Also inlcudes numpy and pandas.
+# Buildah build script for RDKit with Python3.
+# Inlcudes InChi, Avalon, Cairo and Swig support.
 #
-# The RDKIT_BRANCH environment variable must be defined. This defines the RDKit branch to use.
 
 set -e
 
+source ../params.sh
 
-if [ ! -v RDKIT_BRANCH ]; then
-  echo "RDKIT_BRANCH not defined"
+
+if [ ! -v GIT_BRANCH ]; then
+  echo "GIT_BRANCH not defined"
   exit 1
 fi
-echo "Using RDKit branch $RDKIT_BRANCH"
+echo "Using RDKit branch $GIT_BRANCH"
 
 # checkout RDKit source code
 cd /root
 RDBASE=/root/rdkit
 rm -rf $RDBASE
 echo "Checking out RDKit source code"
-git clone -b $RDKIT_BRANCH --single-branch https://github.com/rdkit/rdkit.git
+git clone -b $GIT_BRANCH --single-branch https://github.com/rdkit/rdkit.git
 
 
 mkdir $RDBASE/build
@@ -27,18 +28,23 @@ cd $RDBASE/build
 # build RDKit for python
 echo "Running cmake"
 cmake -Wno-dev\
+  -DLIB_SUFFIX=64\
   -DRDK_INSTALL_INTREE=OFF\
   -DRDK_BUILD_INCHI_SUPPORT=ON\
   -DRDK_BUILD_AVALON_SUPPORT=ON\
   -DRDK_BUILD_CAIRO_SUPPORT=ON\
   -DRDK_BUILD_PYTHON_WRAPPERS=ON\
-  -DRDK_BUILD_SWIG_WRAPPERS=OFF\
+  -DRDK_BUILD_SWIG_WRAPPERS=ON\
   -DCMAKE_INSTALL_PREFIX=/usr\
+  -DCPACK_PACKAGE_RELOCATABLE=OFF\
   ..
 
 echo "Building RDKit"
 nproc=$(getconf _NPROCESSORS_ONLN)
 make -j $(( nproc > 2 ? nproc - 2 : 1 ))
+
+echo "Creating RPMs"
+cpack3 -G RPM
 
 echo "RDKit build complete"
 
