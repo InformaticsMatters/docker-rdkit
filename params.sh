bc473375594b4514877e7f02a5a2a133d020d1e3
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2021-05-05 09:48:32 +0100

    Release_2021_03_1 branch

--- params.sh
+++ params.sh
@@ -1,6 +1,6 @@
-export DOCKER_TAG=Release_2020_09_4
+export DOCKER_TAG=Release_2021_03_1
 export GIT_REPO=https://github.com/rdkit/rdkit.git
-export GIT_BRANCH=Release_2020_09
+export GIT_BRANCH=Release_2021_03
 export GIT_TAG=$DOCKER_TAG
 
 export BASE=informaticsmatters
