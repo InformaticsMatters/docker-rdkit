cb90ea6c210c81b6ea7f96b2591bb4cb142729a5
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2021-12-06 14:35:27 +0000

    Release_2021_09 without cartridge

--- params.sh
+++ params.sh
@@ -1,7 +1,7 @@
-export DOCKER_TAG=Release_2021_03_5
+export DOCKER_TAG=Release_2021_09
 export GIT_REPO=https://github.com/rdkit/rdkit.git
-export GIT_BRANCH=Release_2021_03
-export GIT_TAG=$DOCKER_TAG
+export GIT_BRANCH=$DOCKER_TAG
+unset GIT_TAG
 
 export BASE=informaticsmatters
-echo "DOCKER_TAG=$DOCKER_TAG GIT_BRANCH=$GIT_BRANCH GIT_TAG=$GIT_TAG"
+echo "DOCKER_TAG=$DOCKER_TAG GIT_BRANCH=$GIT_BRANCH"
