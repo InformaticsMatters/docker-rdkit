7363add65c96d2a59643371ae74e6ab5d6ab2b56
Ray Tran <rt@elixirsoftware.co.uk>
2022-01-11 18:22:52 +0000

    Push built images instead of loading them.
    Also comment out some images  I don't need as they take forever
    to build

--- build-debian.sh
+++ build-debian.sh
@@ -4,11 +4,11 @@ set -xe
 
 source params.sh
 
-DBO=${DOCKER_BUILD_OPTS:---no-cache}
+#DBO=${DOCKER_BUILD_OPTS:---no-cache}
 
 # build RDKit
 docker buildx build $DBO -f Dockerfile-build-debian \
-  --platform linux/arm64,linux/amd64 --load \
+  --platform linux/arm64,linux/amd64 --push \
   -t $BASE/rdkit-build-debian:$DOCKER_TAG \
   --build-arg GIT_REPO=$GIT_REPO\
   --build-arg GIT_BRANCH=$GIT_BRANCH\
@@ -24,29 +24,29 @@ docker run -it --rm -u $(id -u)\
   $BASE/rdkit-build-debian:$DOCKER_TAG bash -c 'cp /rdkit/build/*.deb /tohere/debs && cp /rdkit/Code/JavaWrappers/gmwrapper/org.RDKit.jar /rdkit/Code/JavaWrappers/gmwrapper/libGraphMolWrap.so /rdkit/Code/JavaWrappers/gmwrapper/javadoc.tgz /tohere/java'
 
 # build image for python3 on debian
-docker buildx build $DBO -f Dockerfile-python3-debian \
-  --platform linux/arm64,linux/amd64 --load \
-  -t $BASE/rdkit-python3-debian:$DOCKER_TAG \
-  --build-arg DOCKER_TAG=$DOCKER_TAG .
-echo "Built image ${BASE}/rdkit-python3-debian:$DOCKER_TAG"
-
-# build image for java on debian
-docker buildx build $DBO -f Dockerfile-java-debian \
-  --platform linux/arm64,linux/amd64 --load \
-  -t $BASE/rdkit-java-debian:$DOCKER_TAG \
-  --build-arg DOCKER_TAG=$DOCKER_TAG .
-echo "Built image ${BASE}/rdkit-java-debian:$DOCKER_TAG"
-
-# build image for tomcat on debian
-docker buildx build $DBO -f Dockerfile-tomcat-debian \
-  --platform linux/amd64 --load \
-  -t $BASE/rdkit-tomcat-debian:$DOCKER_TAG \
-  --build-arg DOCKER_TAG=$DOCKER_TAG .
-echo "Built image ${BASE}/rdkit-tomcat-debian:$DOCKER_TAG"
+#docker buildx build $DBO -f Dockerfile-python3-debian \
+#  --platform linux/arm64,linux/amd64 --push \
+#  -t $BASE/rdkit-python3-debian:$DOCKER_TAG \
+#  --build-arg DOCKER_TAG=$DOCKER_TAG .
+#echo "Built image ${BASE}/rdkit-python3-debian:$DOCKER_TAG"
+#
+## build image for java on debian
+#docker buildx build $DBO -f Dockerfile-java-debian \
+#  --platform linux/arm64,linux/amd64 --push \
+#  -t $BASE/rdkit-java-debian:$DOCKER_TAG \
+#  --build-arg DOCKER_TAG=$DOCKER_TAG .
+#echo "Built image ${BASE}/rdkit-java-debian:$DOCKER_TAG"
+#
+## build image for tomcat on debian
+#docker buildx build $DBO -f Dockerfile-tomcat-debian \
+#  --platform linux/amd64 --push \
+#  -t $BASE/rdkit-tomcat-debian:$DOCKER_TAG \
+#  --build-arg DOCKER_TAG=$DOCKER_TAG .
+#echo "Built image ${BASE}/rdkit-tomcat-debian:$DOCKER_TAG"
 
 # build image for postgresql cartridge on debian
 docker buildx build $DBO -f Dockerfile-cartridge-debian \
-  --platform linux/arm64,linux/amd64 --load \
+  --platform linux/arm64,linux/amd64 --push \
   -t $BASE/rdkit-cartridge-debian:$DOCKER_TAG \
   --build-arg DOCKER_TAG=$DOCKER_TAG .
 echo "Built image ${BASE}/rdkit-cartridge-debian:$DOCKER_TAG"
