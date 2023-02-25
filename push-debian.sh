4aa8e95a481f5e624a4a35432361f86bfe1af7f5
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2023-02-25 11:10:10 +0000

    Release_2022_09_5

--- push-debian.sh
+++ push-debian.sh
@@ -4,7 +4,6 @@ set -e
 
 source params.sh
 
-docker push $BASE/rdkit-build-debian:$DOCKER_TAG
 docker push $BASE/rdkit-python3-debian:$DOCKER_TAG
 docker push $BASE/rdkit-java-debian:$DOCKER_TAG
 docker push $BASE/rdkit-tomcat-debian:$DOCKER_TAG
