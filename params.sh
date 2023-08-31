b9a017fb96761c5ea419d1a2c07066bc6bcc9fc0
Neil Munro <neil@elixirsoftware.co.uk>
2023-08-31 14:28:39 +0100

    Changed BASE name

--- params.sh
+++ params.sh
@@ -3,5 +3,5 @@ export GIT_REPO=https://github.com/rdkit/rdkit.git
 export GIT_BRANCH=Release_2020_03
 unset GIT_TAG
 
-export BASE=artran
+export BASE=nmunro
 echo "DOCKER_TAG=$DOCKER_TAG GIT_BRANCH=$GIT_BRANCH no tag"
