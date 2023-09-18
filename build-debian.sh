ba2c3a5cc43f6204671869903839a7a79552ea7a
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2023-09-18 11:01:01 +0100

    multi-arch multi-stage builds

--- build-debian.sh
+++ build-debian.sh
@@ -4,56 +4,47 @@ set -xe
 
 source params.sh
 
-#DBO=${DOCKER_BUILD_OPTS:---no-cache}
-
-# build RDKit
-docker buildx build $DBO -f Dockerfile-build-debian \
-  --platform linux/arm64,linux/amd64 \
-  --push \
-  -t $BASE/rdkit-build-debian:$DOCKER_TAG \
-  --build-arg GIT_REPO=$GIT_REPO\
-  --build-arg GIT_BRANCH=$GIT_BRANCH\
-  --build-arg DEB_PACKAGE_DETAILS=$(echo $GIT_BRANCH | cut -d_ -f2 -f3)\
-  --build-arg GIT_TAG=$GIT_TAG .
-
-# copy the packages
-rm -rf artifacts/debian/$DOCKER_TAG
-mkdir -p artifacts/debian/$DOCKER_TAG
-mkdir -p artifacts/debian/$DOCKER_TAG/debs
-mkdir -p artifacts/debian/$DOCKER_TAG/java
-docker run -it --rm -u $(id -u)\
-  -v $PWD/artifacts/debian/$DOCKER_TAG:/tohere:Z\
-  $BASE/rdkit-build-debian:$DOCKER_TAG bash -c 'cp /rdkit/build/*.deb /tohere/debs && cp /rdkit/Code/JavaWrappers/gmwrapper/org.RDKit.jar /rdkit/Code/JavaWrappers/gmwrapper/libGraphMolWrap.so /rdkit/Code/JavaWrappers/gmwrapper/javadoc.tgz /tohere/java'
+DBO=${DOCKER_BUILD_OPTS:---no-cache}
+DOCKERFILE=Dockerfile-debian
 
 # build image for python3 on debian
-docker buildx build $DBO -f Dockerfile-python3-debian \
- --platform linux/arm64,linux/amd64 \
- --push \
- -t $BASE/rdkit-python3-debian:$DOCKER_TAG \
- --build-arg DOCKER_TAG=$DOCKER_TAG .
-echo "Built image ${BASE}/rdkit-python3-debian:$DOCKER_TAG"
+docker buildx build $DBO -f $DOCKERFILE \
+  --platform linux/amd64,linux/arm64 \
+  --target python \
+  -t $BASE/rdkit-python3-debian:$DOCKER_TAG \
+  --build-arg GIT_REPO=$GIT_REPO \
+  --build-arg GIT_BRANCH=$GIT_BRANCH \
+  --push .
+echo "Built images $BASE/rdkit-python3-debian:$DOCKER_TAG"
 
 # build image for java on debian
-docker buildx build $DBO -f Dockerfile-java-debian \
- --platform linux/arm64,linux/amd64 \
- --push \
- -t $BASE/rdkit-java-debian:$DOCKER_TAG \
- --build-arg DOCKER_TAG=$DOCKER_TAG .
-echo "Built image ${BASE}/rdkit-java-debian:$DOCKER_TAG"
+docker buildx build $DBO -f $DOCKERFILE \
+  --platform linux/amd64,linux/arm64 \
+  --target java \
+  -t $BASE/rdkit-java-debian:$DOCKER_TAG \
+  --build-arg GIT_REPO=$GIT_REPO \
+  --build-arg GIT_BRANCH=$GIT_BRANCH \
+  --push .
+echo "Built images $BASE/rdkit-java-debian:$DOCKER_TAG"
 
 # build image for tomcat on debian
-docker buildx build $DBO -f Dockerfile-tomcat-debian \
- --platform linux/arm64,linux/amd64 \
- --push \
- -t $BASE/rdkit-tomcat-debian:$DOCKER_TAG \
- --build-arg DOCKER_TAG=$DOCKER_TAG .
-echo "Built image ${BASE}/rdkit-tomcat-debian:$DOCKER_TAG"
+docker buildx build $DBO -f $DOCKERFILE \
+  --platform linux/amd64 \
+  --target tomcat \
+  -t $BASE/rdkit-tomcat-debian:$DOCKER_TAG \
+  --build-arg GIT_REPO=$GIT_REPO \
+  --build-arg GIT_BRANCH=$GIT_BRANCH \
+  --push .
+echo "Built images $BASE/rdkit-tomcat-debian:$DOCKER_TAG"
 
-# build image for postgresql cartridge on debian
-docker buildx build $DBO -f Dockerfile-cartridge-debian \
-  --platform linux/arm64,linux/amd64 \
-  --push \
+# build image for cartridge on debian
+docker buildx build $DBO -f $DOCKERFILE \
+  --platform linux/amd64,linux/arm64 \
+  --target cartridge \
   -t $BASE/rdkit-cartridge-debian:$DOCKER_TAG \
-  --build-arg DOCKER_TAG=$DOCKER_TAG .
-echo "Built image ${BASE}/rdkit-cartridge-debian:$DOCKER_TAG"
+  --build-arg GIT_REPO=$GIT_REPO \
+  --build-arg GIT_BRANCH=$GIT_BRANCH \
+  --push .
+echo "Built images $BASE/rdkit-cartridge-debian:$DOCKER_TAG"
+
 
