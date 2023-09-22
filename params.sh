8ea1bdebb298b8aea2628fe95a02f6c5786c5da5
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2023-09-22 20:14:10 +0100

    fixed cartridge build

--- params.sh
+++ params.sh
@@ -1,4 +1,4 @@
-export DOCKER_TAG=Release_2023_03_1
+export DOCKER_TAG=Release_2023_03_3
 export GIT_REPO=https://github.com/rdkit/rdkit.git
 export GIT_BRANCH=Release_2023_03
 export GIT_TAG=$DOCKER_TAG
