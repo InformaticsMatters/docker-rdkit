ae4c73e24f56cd78e945e8cc8caede6401397ea2
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2024-11-13 07:17:47 +0000

    Release_2024_03_4

--- params.sh
+++ params.sh
@@ -1,4 +1,4 @@
-export DOCKER_TAG=Release_2024_03_3
+export DOCKER_TAG=Release_2024_03_4
 export GIT_REPO=https://github.com/rdkit/rdkit.git
 export GIT_BRANCH=Release_2024_03
 export GIT_TAG=$DOCKER_TAG
