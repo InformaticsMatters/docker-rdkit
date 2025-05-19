bc192a3487dabe868815f67c95646a8cf757c4e2
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2025-05-19 11:25:47 +0100

    Release_2024_09_3

--- build-debian.sh
+++ build-debian.sh
@@ -4,12 +4,13 @@ set -xe
 
 source params.sh
 
-DBO=${DOCKER_BUILD_OPTS:---no-cache}
+DBO=''
+PLATFORMS=${DOCKER_PLATFORMS:-linux/amd64,linux/arm64}
 DOCKERFILE=Dockerfile-debian
 
 # build image for python3 on debian
 docker buildx build $DBO -f $DOCKERFILE \
-  --platform linux/amd64,linux/arm64 \
+  --platform $PLATFORMS \
   --target python \
   -t $BASE/rdkit-python3-debian:$DOCKER_TAG \
   --build-arg GIT_REPO=$GIT_REPO \
@@ -19,7 +20,7 @@ echo "Built images $BASE/rdkit-python3-debian:$DOCKER_TAG"
 
 # build image for java on debian
 docker buildx build $DBO -f $DOCKERFILE \
-  --platform linux/amd64,linux/arm64 \
+  --platform $PLATFORMS \
   --target java \
   -t $BASE/rdkit-java-debian:$DOCKER_TAG \
   --build-arg GIT_REPO=$GIT_REPO \
@@ -29,7 +30,7 @@ echo "Built images $BASE/rdkit-java-debian:$DOCKER_TAG"
 
 # build image for tomcat on debian
 docker buildx build $DBO -f $DOCKERFILE \
-  --platform linux/amd64,linux/arm64 \
+  --platform $PLATFORMS \
   --target tomcat \
   -t $BASE/rdkit-tomcat-debian:$DOCKER_TAG \
   --build-arg GIT_REPO=$GIT_REPO \
@@ -39,7 +40,7 @@ echo "Built images $BASE/rdkit-tomcat-debian:$DOCKER_TAG"
 
 # build image for cartridge on debian
 docker buildx build $DBO -f $DOCKERFILE \
-  --platform linux/amd64,linux/arm64 \
+  --platform $PLATFORMS \
   --target cartridge \
   -t $BASE/rdkit-cartridge-debian:$DOCKER_TAG \
   --build-arg GIT_REPO=$GIT_REPO \
