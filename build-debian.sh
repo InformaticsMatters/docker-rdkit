ee8ddd94d0a133a1f670f527fe68632a2f401687
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2025-06-13 14:14:54 +0100

    tweaks from master

--- build-debian.sh
+++ build-debian.sh
@@ -5,8 +5,9 @@ set -xe
 source params.sh
 
 DBO=${DOCKER_BUILD_OPTS:---no-cache}
-PLATFORM=${DOCKER_PATFORM:-linux/amd64,linux/arm64}
+PLATFORM=${DOCKER_PLATFORM:-linux/amd64,linux/arm64}
 DOCKERFILE=Dockerfile-debian
+nproc=$(getconf _NPROCESSORS_ONLN) && N_CORES=${DOCKER_N_CORES:-$(( nproc > 2 ? nproc - 2 : 1 ))}
 
 # build image for python3 on debian
 docker buildx build $DBO -f $DOCKERFILE \
@@ -15,6 +16,7 @@ docker buildx build $DBO -f $DOCKERFILE \
   -t $BASE/rdkit-python3-debian:$DOCKER_TAG \
   --build-arg GIT_REPO=$GIT_REPO \
   --build-arg GIT_BRANCH=$GIT_BRANCH \
+  --build-arg N_CORES=$N_CORES \
   --push .
 echo "Built images $BASE/rdkit-python3-debian:$DOCKER_TAG"
 
